ed6745ec3418b869ececdc2b75e3caaad8e35380
Yakov Medvedenko <53426310+mishkanya@users.noreply.github.com>
2024-02-22 15:45:05 +0300

    Init projects

---
+++ ArendApp.App/ArendApp.App/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ArendApp.App.Services;
+using ArendApp.App.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ArendApp.App
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ArendApp.App/ArendApp.App/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ArendApp.App.ViewModels;
+using ArendApp.App.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ArendApp.App
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
