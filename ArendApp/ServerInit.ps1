11e4e159022aef053510d1f4d329b34844b9c770
Yakov Medvedenko <53426310+mishkanya@users.noreply.github.com>
2024-03-25 19:37:53 +0300

    21321321321312313123

--- ArendApp/ServerInit.ps1
+++ ArendApp/ServerInit.ps1
@@ -25,7 +25,7 @@
 #}
 
 
-# https://api.telegra.ph/editPage/ServerUrl-03-07?access_token=98815802fe8b87c09b320647fee62577449b44e35cb6b3474599c5854f86&title=ServerUrl&content=[{"tag":"p","children":["https://d163-80-80-195-185.ngrok-free.app"]}]&return_content=true
+# https://api.telegra.ph/editPage/ServerUrl-03-07?access_token=98815802fe8b87c09b320647fee62577449b44e35cb6b3474599c5854f86&title=ServerUrl&content=[{"tag":"p","children":["https://548d-80-80-194-105.ngrok-free.app"]}]&return_content=true
 
 
 
